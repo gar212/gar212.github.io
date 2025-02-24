b4a5aec9fb21f7763c5a37f13eed5a0dee5dc39f
gar212 <gartoyou@gmail.com>
2025-02-24 20:18:42 +1300

    Change BG color

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Background: React.FC = () => {
 
   return (
-    <div className='bg-[#20201f] h-screen w-screen z-0 fixed top-0'>
+    <div className='bg-[#19191c] h-screen w-screen z-0 fixed top-0'>
       <div className='noise'></div>
     </div>
   );
